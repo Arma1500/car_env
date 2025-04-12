a6cffc1b583f97e1c85803dbdf3a9302eb8df8ef
Arhma Baig <Arhma.Baig@student.uts.edu.au>
2025-04-12 14:34:29 +1000

    using -dist reward instead

--- simple_driving/envs/simple_driving_env.py
+++ simple_driving/envs/simple_driving_env.py
@@ -75,9 +75,9 @@ class SimpleDrivingEnv(gym.Env):
                                   (car_ob[1] - self.goal[1]) ** 2))
         #dist_to_goal = math.sqrt(((carpos[0] - goalpos[0]) ** 2 +
         #                          (carpos[1] - goalpos[1]) ** 2))
-        reward = max(self.prev_dist_to_goal - dist_to_goal, 0)
+        #reward = max(self.prev_dist_to_goal - dist_to_goal, 0)
+        reward = -dist_to_goal
         print('reward: ' + str(reward))
-        #reward = -dist_to_goal
         self.prev_dist_to_goal = dist_to_goal
 
         # Done by reaching goal
